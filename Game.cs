8b41b6aee9c4eb1aa5da2b2899c3397144e1d4a3
Maira Vecele <mairavecele6@gmail.com>
2020-07-11 16:28:28 +0300

    version 16:28

---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheSnakeGame
+{
+    public partial class Game : Form
+    {
+        Area area = new Area();
+        Snake snake = new Snake();
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            this.Height = 560;
+            this.Width = 560;
+
+            this.Controls.Add(area);
+            area.Top = 80;
+            area.Left = 80;
+            //area.Location = new Point(80, 80);
+
+            //adding snake body
+            snake.Render(this);
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheSnakeGame
+{
+    class Snake
+    {
+        List<PictureBox> snakePixels = new List<PictureBox>();
+
+        public Snake()
+        {
+            InitializeSnake();
+        }
+
+        private void InitializeSnake()
+        {
+            PictureBox snakePixel = new PictureBox();
+            snakePixel.Height = 20;
+            snakePixel.Width = 20;
+            snakePixel.BackColor = Color.Green;
+            snakePixels.Add(snakePixel);
+        }
+
+        public void Render(Form form)
+        {
+            foreach(var sp in snakePixels)
+            {
+                form.Controls.Add(sp);
+            }
+        }
+    }
+}
